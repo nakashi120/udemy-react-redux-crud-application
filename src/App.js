e62ca70729ca9063a4384ae692d2b34af54fa968
nakashi120 <nakashi120@gmail.com>
2021-02-21 22:34:21 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react'
 const App = () =>  {
   return (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=>{console.log("I am checked.")}}/>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   );
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
